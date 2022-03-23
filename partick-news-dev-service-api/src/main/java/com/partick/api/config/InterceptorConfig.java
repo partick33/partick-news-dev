d730d2b882c3d4d0e629de7abae8a3b0b2b5d78c
patrick <775210145@qq.com>
2022-03-23 09:25:53 +0800

    增加请求拦截器，对频繁获取短信验证码进行拦截

---
+++ partick-news-dev-service-api/src/main/java/com/partick/api/config/InterceptorConfig.java
@@ -0,0 +1,25 @@
+package com.partick.api.config;
+
+import com.partick.api.interceptor.PassportInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author partick_peng
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PassportInterceptor passportInterceptor() {
+        return new PassportInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor())
+                .addPathPatterns("/passport/getSMSCode");
+    }
+}
---
+++ partick-news-dev-service-api/src/main/java/com/partick/api/interceptor/PassportInterceptor.java
@@ -0,0 +1,68 @@
+package com.partick.api.interceptor;
+
+import com.partick.common.utils.IPUtil;
+import com.partick.common.utils.RedisOperator;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.partick.api.controller.BaseController.MOBILE_SMSCODE;
+
+/**
+ * @author partick_peng
+ */
+public class PassportInterceptor implements HandlerInterceptor {
+    @Resource
+    private RedisOperator redisOperator;
+
+    /**
+     * 拦截请求，访问Controller之前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return false:请求被拦截 true:放行
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取用户IP
+        String userIp = IPUtil.getRequestIp(request);
+        //查看缓存是否存在
+        boolean isExist = redisOperator.keyIsExist(MOBILE_SMSCODE + ":" + userIp);
+        if (isExist) {
+            System.out.println("短信发送频率太大");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 请求访问到Controller之后，渲染视图之前
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 请求访问到Controller之后，渲染视图之后
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
